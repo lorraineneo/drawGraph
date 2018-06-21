322e33c743d8b30fb3deb49f4d35df9e148bf782
lorraineneo <lorraineneo@gmail.com>
2018-06-21 16:30:27 +0800

    Update drawGraph.py

--- drawGraph.py
+++ drawGraph.py
@@ -1,9 +1,7 @@
 # https://github.com/lorraineneo/drawGraph.git
 import graphviz
 
-def generateTree(tree):
-  
-  def listtree(tree):
+def listtree(tree):
     '''
        Generates a list structure that represents the binary tree
        E.g.
@@ -20,34 +18,34 @@ def generateTree(tree):
     if tree:
         sVal.append(str(tree.getRootVal()))
         if tree.getLeft() != None:
-          sVal.append(listtree(tree.getLeft()))
+            sVal.append(listtree(tree.getLeft()))
 
         if tree.getRight() != None:
           sVal.append(listtree(tree.getRight()))
     return sVal
 
-  def add_edge(graph, source, dest):
+def add_edge(graph, source, dest):
     graph.edge(source, dest)
     
 
-  def first_flat(tree):
+def first_flat(tree):
     if isinstance(tree, list):
       return first_flat(tree[0])
     else:
       return tree
 
-  def find_edges(graph, tree):
+def find_edges(graph, tree):
     if isinstance(tree, list):
       source = tree[0]
       graph.node(source)
       for dest in tree[1:]:
         add_edge(graph,source, first_flat(dest))
         find_edges(graph, dest)
-
-  if isinstance(tree, BinaryTree):
-    exptree = listtree(tree) #converts Binary Tree to a list structure first
-    graph = graphviz.Digraph()
-    find_edges(graph, exptree)
-    return graph
-  else:
-    return None
+def generateTree(tree):
+    if isinstance(tree, BinaryTree):
+      exptree = listtree(tree) #converts Binary Tree to a list structure first
+      graph = graphviz.Digraph()
+      find_edges(graph, exptree)
+      return graph
+    else:
+        return None
